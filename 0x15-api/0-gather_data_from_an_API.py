0446356871ae6d26995c1f3f778e6ca7074e434e
raytchellee <amakaoforkaja@gmail.com>
2023-11-27 12:29:05 -0800

    check

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""gets info by id"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com/"
+    person = requests.get(link + "users/{}".format(argv[1])).json()
+    tasks = requests.get(link + "todos", params={"userId": argv[1]}).json()
+
+    done = [i.get("title") for i in tasks if i.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        person.get("name"), len(done), len(tasks)))
+    [print("\t {}".format(idx)) for idx in done]
