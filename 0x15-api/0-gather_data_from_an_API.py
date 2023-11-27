cc1b513ec34ec38e0f41cce92367781d5ca2e404
raytchellee <amakaoforkaja@gmail.com>
2023-11-27 12:32:14 -0800

    task check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """gets info by id"""
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     link = "https://jsonplaceholder.typicode.com/"
