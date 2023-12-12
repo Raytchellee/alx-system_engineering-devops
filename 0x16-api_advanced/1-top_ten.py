67a16d4c0f6275c2acb48573d4146376ce147dcb
raytchellee <amakaoforkaja@gmail.com>
2023-12-12 09:15:43 -0800

    check

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts """
     link = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     h = {'User-agent': '0x16-api_advanced/0.0.1 (by /u/raytchellee)'}
-    p={"limit": 10}
+    p = {"limit": 10}
 
     res = requests.get(link, headers=h,
                        params=p, allow_redirects=False)
