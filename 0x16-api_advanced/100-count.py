a78c2a4ab012f4e8cb7f6465b674a082db7b8f4a
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-12-12 20:05:13 +0100

    final tests!!!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,5 @@
 #!/usr/bin/python3
 """Gets total words in hot posts of a subreddit"""
-import requests
-
-
-def count_words(subreddit, keywords, cont='', obj={}):
-    """
-    Gets total words in hot posts of a subreddit recursively.
-    If no result return nothing
-    """
-
-    if not obj:
-        for k in keywords:
-            if k.lower() not in obj:
-                obj[k.lower()] = 0
-
-    if cont is None:
-        count = sorted(obj.items(), key=lambda y: (-y[1], y[0]))
-        for w, c in count:
-            if c:
-                print('{}: {}'.format(w, c))
-        return None
-
-    link = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    h = {'user-agent': '0x16-api_advanced/0.0.1 (by /u/raytchellee)'}
-    p = {'limit': 100, 'after': cont}
-
-    res = requests.get(link,
-                            headers=h,
-                            params=p,
-                            allow_redirects=False)
-
-    if res.status_code != 200:
-        return None
-
-    try:
-        hot_posts = res.json()['data']['children']
-        next_token = res.json()['data']['after']
-        for p in hot_posts:
-            title = p['data']['title']
-            lc = [word.lower() for word in title.split(' ')]
-
-            for keyword in obj.keys():
-                obj[keyword] += lc.count(keyword)
-
-    except Exception:
-        return None
-
-    count_words(subreddit, keywords, next_token, obj)
-
-#!/usr/bin/python3
-"""Gets total words in hot posts of a subreddit"""
 from requests import get
 
 
