61aaacd3b01bda73fe7f6b6d1c188e3e4f246576
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-12-12 21:06:44 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,25 +3,22 @@
 from requests import get
 
 
-def count_words(subreddit, target_words, cont="", obj={}):
+def count_words(subreddit, target_words, cont="", obj=None):
     """
     Gets total words in hot posts of a subreddit recursively.
     If no result return nothing
     """
-
-    if not obj:
-        for word in target_words:
-            obj[word] = 0
+    if obj is None:
+        obj = {word: 0 for word in target_words}
 
     if cont is None:
-        target_words = [[a, b] for a, b in obj.items()]
-        target_words = sorted(target_words, key=lambda y: (-y[1], y[0]))
-        for word in target_words:
-            if word[1]:
-                print("{}: {}".format(word[0].lower(), word[1]))
+        target_words_sorted = sorted(obj.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in target_words_sorted:
+            if count:
+                print(f"{word.lower()}: {count}")
         return None
 
-    link = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    link = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     p = {
         'limit': 100,
         'after': cont
@@ -29,10 +26,14 @@ def count_words(subreddit, target_words, cont="", obj={}):
     h = {
         'user-agent': '0x16-api_advanced/0.0.1 (by /u/raytchellee)'
     }
-    res = get(link, headers=h, params=p, allow_redirects=False)
+    res = get(link,
+              headers=h,
+              params=p,
+              allow_redirects=False)
 
     if res.status_code != 200:
         return None
+
     try:
         json_data = res.json()
     except ValueError:
@@ -41,15 +42,15 @@ def count_words(subreddit, target_words, cont="", obj={}):
     try:
         result = json_data.get("data")
         props = result.get("children")
-        next = result.get("after")
+        next_page = result.get("after")
         for prop in props:
             post = prop.get("data")
-            t = post.get("title")
-            lc = [word.lower() for word in t.split(' ')]
+            title = post.get("title")
+            lowercased_words = [word.lower() for word in title.split(' ')]
 
             for word in target_words:
-                obj[word] += lc.count(word.lower())
+                obj[word] += lowercased_words.count(word.lower())
     except:
         return None
 
-    count_words(subreddit, target_words, next, obj)
+    count_words(subreddit, target_words, next_page, obj)
