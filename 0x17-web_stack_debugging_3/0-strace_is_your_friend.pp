9c65b3df2696e8776cfe3234e67abd8771959a4f
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-12-13 20:20:56 +0100

    testing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixed error using stace
 
 exec { 'Fix php error':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
 }
