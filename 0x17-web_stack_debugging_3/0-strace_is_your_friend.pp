eb00657eb59b11bbca8cb470d36e4880ee371630
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-12-13 20:18:27 +0100

    check

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixed error using stace
 
-exec { 'fixed-php-error':
-  path    => '/bin',
-  command => 'sed -i 's/phpp/php/g'  /var/www/html/wp-settings.php';
+exec { 'Fix php error':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
